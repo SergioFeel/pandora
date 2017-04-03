5c65887845745607b08b8de9cd28e3cccbac1e3d
SergioFeel <karnelliani@gmail.com>
2017-04-03 15:56:47 +0300

    тут файл с норм кодом - сейчас буду проверять историю

--- ex10001err.js
+++ ex10001err.js
@@ -1,5 +1,3 @@
-/* кривой кусок кода */
-
 var ourCoords = {
 	latitude: 47.624851,
 	longitude: -122.52099
@@ -7,35 +5,64 @@ var ourCoords = {
 
 window.onload = getMyLocation;
 
-function getMyLocation() {
+function getMyLocation() { 
 	if (navigator.geolocation) {
 		navigator.geolocation.getCurrentPosition(displayLocation, displayError);
 	} else {
-		alert('oops, no geolocation support');
+		alert('Oops, no geolocation support');
 	}
 }
 
 function displayLocation(position) {
-	var latitude = position.coords.latitude;
+	var latitude = position.coords.latitude; 
 	var longitude = position.coords.longitude;
 	
 	var div = document.getElementById('location');
-	div.innerHTML = 'you`re at latitude: ' + latitude + ' , longitude ' + longitude;
+	div.innerHTML = 'You`re at Latitude: ' + latitude + ' , Longitude: ' + longitude; 
 }
 
-function diplayError(error) {
-	var errorTypes = {
-		0: 'unknown error',
-		1: 'permission denied by user',
-		2: 'position isn`t available',
-		3: 'request timed out'
+function displayError(error) { 
+	var errorTypes = { 
+		0: 'Unknown error',
+		1: 'Permission denied by user', 
+		2: 'Position isn`t available',
+		3: 'Request timed out'
 	};
-	var errorMessage = errorTypes[error.code];
+	var errorMessage = errorTypes[error.code]; 
 	if (error.code == 0 || error.code == 2) {
-		errorMessage = errorMessage + ' ' + error.message;
+		errorMessage = errorMessage + ' ' + error.message; 
 	}
 	var div = document.getElementById('location');
-	div.innerHTML = errorMessage;
+	div.innerHTML = errorMessage; 
 }
 
+function computeDistance(startCoords, destCoords) { //Функция принимает две точки координат - нач. и конеч. и возвращает расстояние между ними в Км
+	var startLatRads = degreesToRadians(startCoords.latitude);
+	var startLongRads = degreesToRadians(startCoords.longitude);
+	var destLatRads = degreesToRadians(destCoords.latitude);
+	var destLongRads = degreesToRadians(destCoords.longitude);
+	
+	var Radius = 6371; //радиус Земли в Километрах
+	var distance = Math.acos(Math.sin(startLatRads) * Math.sin(destLatRads) +
+					Math.cos(startLatRads) * Math.cos(destLatRads) *
+					Math.cos(startLongRads - destLongRads)) * Radius;
 
+return distance;					
+}
+
+function degreesToRadians(degrees) {
+	var radians = (degrees * Math.PI)/180;
+	return radians;
+}
+
+function displayLocation(position) {
+	var latitude = position.coords.latitude;
+	var longitude= position.coords.longitude;
+	
+	var div = document.getElementById('location');
+	div.innerHTML = 'you`reat latitude: ' + latitude + ' , longitude: ' + longitude;
+	
+	var km = computeDistance(position.coords, ourCoords); //передаем координаты вышего и нашего местоположения функции computeDistance
+	var distance = document.getElementById('distance');
+	distance.innerHTML = 'you`re ' + km + 'km from the WickedlySmart HQ'
+}
